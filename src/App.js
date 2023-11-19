cecb796a66f8b03cf63f553e184e19983b0dad79
FatimazahraeTassouliAddad <fatimazahrae.tasouli@gmail.com>
2023-11-19 21:19:44 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './index.css';
+import { useState,useEffect} from 'react' ;
+import axios from 'axios' ;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [dataArray, setDataArray] = useState ([]) ;
+
+const [page, setPage] = useState (1) ;
+ 
+useEffect (() => {
+ axios.get(`https://jsonplaceholder.typicode.com/todos?_page=${page}&_limit=10`)
+ .then ( (response) =>
+ {setDataArray (response .data) })
+}, [page])
+
+const prev = () => {
+  setPage (page + 1)
+}
+const next = () => {
+if (page > 1) {
+  setPage (page - 1)
+  }
+}
+return (
+<center>
+<div className="container" >
+
+  <h1 className='titre'> API TP TXTE </h1>
+
+  <ul>
+       {dataArray.map((el,id)=>( <li>{el.title}</li>))}
+   </ul>
+   <button className='css' onClick={prev}> Previous</button>
+   <button className='css1' onClick={next}> Next</button>
+   </div>
+</center>
+)
 }
 
-export default App;
+export default App ;
